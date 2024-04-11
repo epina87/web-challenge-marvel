7dae937872633cd358afb582a2d91579a2e36bc9
e.pina <e.pina@ultramarmanagement.com>
2024-04-11 17:19:36 +0200

    reload test

---
+++ src/components/Header.test.jsx
@@ -0,0 +1,23 @@
+mport React from 'react';
+import { render, screen } from '@testing-library/react';
+import { CharacterContext } from '../context/CharacterContext';
+import Header from './Header';
+
+const mockContextValue = {
+  favorites: [],
+  setFavorites: jest.fn(),
+};
+
+const renderComponent = (props) => {
+  return render(
+    <CharacterContext.Provider value={mockContextValue}>
+      <Header {...props} />
+    </CharacterContext.Provider>
+  );
+};
+
+describe('Header component', () => {
+  it('renders', () => {
+    renderComponent()
+  })
+});
